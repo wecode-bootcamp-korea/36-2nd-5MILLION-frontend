a22b2ddddff9c136459d5c654c28f0395d2aaaa4
kimjunho <jh241093@gmail.com>
2022-08-30 20:23:25 +0900

    ADD instructors [통신까지 완료]

--- src/pages/Instructor/Instructor.js
+++ src/pages/Instructor/Instructor.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import * as S from './Styled.Instructor';
 
 const Instructor = () => {
-  return <div>Instructor</div>;
-};
+  const [instructorList, setInstructorList] = useState();
 
+  useEffect(() => {
+    const getInstructor = async () => {
+      const gets = await axios('http://10.58.4.122:3000/instructors');
+      setInstructorList(gets.data.instructors);
+    };
+    getInstructor();
+  }, []);
+  return (
+    <>
+      <S.InstructorTitle>INSTRUCTORS</S.InstructorTitle>
+      <S.SubTitle>안무가</S.SubTitle>
+      <S.InstructorFlex>
+        {instructorList?.map(({ id, profile_image_url, name_en }) => (
+          <Link to={`/instructorDetail/${id}`} key={id}>
+            <S.Square>
+              <S.ImgBox color={id}>
+                <S.Img src={profile_image_url} alt={name_en} />
+              </S.ImgBox>
+              <S.TestBox>
+                <S.TestLine>{name_en}</S.TestLine>
+                <S.TestNoLine />
+                <S.TestLine>→</S.TestLine>
+              </S.TestBox>
+            </S.Square>
+          </Link>
+        ))}
+      </S.InstructorFlex>
+    </>
+  );
+};
 export default Instructor;
