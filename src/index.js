6b7dd419001f37dffb85ff92c30323ef774f48e1
CodyMan0 <hys83751952@gmail.com>
2022-08-30 17:24:23 +0900

    fix review

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 import Router from './Router';
+import variables from './styles/variable';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider theme={theme}>
+  <ThemeProvider theme={{ style: theme, variables }}>
     <GlobalStyle />
     <Router />
   </ThemeProvider>
