c26633cb0ee12bfb390d7b2b096a2837b49bbe21
kimjuno97 <jh241093@gamil.com>
2022-09-05 21:29:04 +0900

    detail Page 통신 구현

--- src/pages/InstructorDetail/ScheduleBox/Styled.ScheduleBox.js
+++ src/pages/InstructorDetail/ScheduleBox/Styled.ScheduleBox.js
@@ -5,26 +5,23 @@ export const Wrapper = styled.div`
   flex-direction: row;
   justify-content: space-around;
   border-bottom: 0.5px solid white;
-  font-size: 1.6rem;
+  font-size: 2.2rem;
 
   & > div {
-    width: 17%;
+    width: 20%;
     text-align: center;
     padding: 3rem 0;
   }
 `;
 
-export const ScheduleContents = styled.div`
-  width: 10rem;
-`;
-
 export const BookNow = styled.button`
   width: 10rem;
-  height: 2.8rem;
+  height: 3.6rem;
   margin: 3rem 0;
   border: 0;
-  border-radius: 1.7rem;
+  border-radius: 2rem;
   background-color: white;
+  font-size: 1.4rem;
   font-weight: 800;
   cursor: ${({ disabled }) => (disabled ? '' : 'pointer')};
   opacity: ${({ disabled }) => (disabled ? 0.2 : 1)};
