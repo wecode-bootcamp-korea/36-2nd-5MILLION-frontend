933163f6078743c98611edba95b6a848528e5372
kimjuno97 <jh241093@gamil.com>
2022-09-05 21:29:04 +0900

    detail Page 통신 구현

--- src/pages/InstructorDetail/InstructorDetail.js
+++ src/pages/InstructorDetail/InstructorDetail.js
@@ -5,23 +5,37 @@ import axios from 'axios';
 import * as S from './Styled.InstructorDetail';
 
 const InstructorDetail = () => {
-  const [instructor, setInstructor] = useState();
-  const [showSchedule, setShowSkedule] = useState();
+  const [instructor, setInstructor] = useState([]);
+  const [showSchedule, setShowSkedule] = useState({});
   const { id } = useParams();
+
+  const showScheduleList = [
+    showSchedule.Mon,
+    showSchedule.Tue,
+    showSchedule.Wed,
+    showSchedule.Thu,
+    showSchedule.Fri,
+    showSchedule.Sat,
+    showSchedule.Sun,
+  ];
 
   useEffect(() => {
     const getInstructor = async () => {
-      const gets = await axios(`../data/detail/detail${id}.json`);
-      setInstructor(gets.data);
-    };
-
-    const getschedule = async () => {
-      const gets = await axios('../data/detail/schedule/schedule.json');
-      setShowSkedule(gets.data);
+      const gets = await axios(`http://10.58.4.122:3001/instructors/${id}`);
+      setInstructor(gets.data.instructor[0]);
     };
     getInstructor();
-    getschedule();
   }, [id]);
+
+  useEffect(() => {
+    const getschedule = async () => {
+      const gets = await axios(
+        `http://10.58.5.244:3001/schedule?instructor=${instructor.name}`
+      );
+      setShowSkedule(gets.data);
+    };
+    getschedule();
+  }, [instructor.name]);
 
   const navigate = useNavigate();
 
@@ -33,8 +47,8 @@ const InstructorDetail = () => {
     instructor && (
       <>
         <S.TitleBox>
-          <S.EngTitle>{instructor.englishName}</S.EngTitle>
-          <S.KoreaTitle>{instructor.koreanName}</S.KoreaTitle>
+          <S.EngTitle>{instructor.name_en}</S.EngTitle>
+          <S.KoreaTitle>{instructor.name}</S.KoreaTitle>
           <S.Ul>
             {UL_DATA.map((liData, idx) => (
               <li key={idx}>{liData}</li>
@@ -42,11 +56,11 @@ const InstructorDetail = () => {
           </S.Ul>
         </S.TitleBox>
         <S.ClassTitle fontSize="4.5rem">
-          {`${instructor.englishName}'S UPCOMING CLASSES`}
+          {`${instructor.name_en}'S UPCOMING CLASSES`}
         </S.ClassTitle>
         <S.ClassTitle fontSize="3rem">예정된 수업</S.ClassTitle>
         <S.ScheduleBox>
-          {showSchedule?.map((oneSchedule, idx) => (
+          {showScheduleList?.map((oneSchedule, idx) => (
             <ScheduleBox key={idx} oneSchedule={oneSchedule} />
           ))}
         </S.ScheduleBox>
--- src/pages/InstructorDetail/ScheduleBox/ScheduleBox.js
+++ src/pages/InstructorDetail/ScheduleBox/ScheduleBox.js
@@ -1,18 +1,69 @@
 import * as S from './Styled.ScheduleBox';
 
 const ScheduleBox = ({ oneSchedule }) => {
-  const { schedule, time, classes, level, spot, deadline } = oneSchedule;
+  const scheduleList = [];
 
-  return (
-    <S.Wrapper>
-      <div>{schedule}</div>
-      <div>{time}</div>
-      <div>{classes}</div>
-      <div>{level}</div>
-      <div>{spot}</div>
-      <S.BookNow disabled={deadline}>BOOK NOW</S.BookNow>
-    </S.Wrapper>
+  oneSchedule?.map(
+    ({
+      dayofweek,
+      day,
+      startTime,
+      endTime,
+      classes,
+      classType,
+      bookedCount,
+      quantity,
+    }) =>
+      scheduleList.push({
+        dayofweek,
+        day,
+        startTime,
+        endTime,
+        classes,
+        classType,
+        bookedCount,
+        quantity,
+      })
+  );
+
+  return scheduleList.map(
+    (
+      { dayofweek, day, startTime, endTime, classType, quantity, bookedCount },
+      idx
+    ) => {
+      const spots = quantity - Number(bookedCount);
+      const deadline = spots === 0 ? true : false;
+      const classTypes = classType.charAt(0).toUpperCase() + classType.slice(1);
+      return (
+        <S.Wrapper key={idx}>
+          <div>
+            {CALENDAR[dayofweek]}-{MONTH}
+            &nbsp;{day},&nbsp;{YEAR}
+          </div>
+          <div>
+            {startTime} - {endTime} PM
+          </div>
+          <div>{classTypes}</div>
+          <div>{`${spots} Spots Left`}</div>
+          <S.BookNow disabled={deadline}>BOOK NOW</S.BookNow>
+        </S.Wrapper>
+      );
+    }
   );
 };
 
 export default ScheduleBox;
+
+const CALENDAR = {
+  Mon: 'Monday',
+  Tue: 'Tuesday',
+  Wed: 'Wednesday,',
+  Thu: 'Tuesday',
+  Fri: 'Friday',
+  Sat: 'Saturday',
+  Sun: 'Sunday',
+};
+
+const NOW = new Date();
+const MONTH = NOW.toLocaleString('en-US', { month: 'long' });
+const YEAR = NOW.getFullYear();
