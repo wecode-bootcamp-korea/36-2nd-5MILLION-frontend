f341da5a4e85fd0592597646cba257f321fa11db
kimjuno97 <jh241093@gamil.com>
2022-09-05 19:54:30 +0900

    add: feature/footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import * as S from './Styled.Footer';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <S.FooterTop>
+        {FOOTER_TOP_DATA.map(({ KOR_TITLE, EN_TITLE }, idx) => (
+          <S.FooterDiv key={idx}>
+            <S.FooterTitle size="2.5rem">{KOR_TITLE}</S.FooterTitle>
+            <S.FooterTitle size="7rem">{EN_TITLE}</S.FooterTitle>
+          </S.FooterDiv>
+        ))}
+      </S.FooterTop>
+      <S.FooterUnder>
+        <S.FooterUderTitle>
+          {FOOTER_UNDER_DATA.map(({ TITLE, SIZE }, idx) => (
+            <S.FooterUderTitle key={idx} size={SIZE}>
+              {TITLE}
+            </S.FooterUderTitle>
+          ))}
+        </S.FooterUderTitle>
+      </S.FooterUnder>
+      <S.FooterBottom>
+        {FOOTER_BOTTOM_DATA.map((data, idx) => (
+          <div key={idx}>{data}</div>
+        ))}
+      </S.FooterBottom>
+    </>
+  );
 };
 
 export default Footer;
+
+const FOOTER_TOP_DATA = [
+  { KOR_TITLE: '자주하는 질문', EN_TITLE: 'FAQ' },
+  { KOR_TITLE: '문의하기', EN_TITLE: 'CONTACT US' },
+  { KOR_TITLE: '에이전시', EN_TITLE: 'AGENCY  →' },
+  { KOR_TITLE: '채용', EN_TITLE: 'RECRUITMENT  →' },
+];
+
+const FOOTER_UNDER_DATA = [
+  { TITLE: '1MILLION', SIZE: '15rem' },
+  { TITLE: 'DANCE STUDIO', SIZE: '7rem' },
+  { TITLE: 'SEOUL', SIZE: '4rem' },
+  { TITLE: '+82 2 512 6756', SIZE: '3rem' },
+  { TITLE: 'STUDIO@1MILLIONDANCE.COM', SIZE: '3rem' },
+];
+
+const FOOTER_BOTTOM_DATA = [
+  'ⓒ 2021 1MILLION Inc. All Rights Reserved.',
+  'Terms of Service',
+  'Privacy Policy',
+];
---
+++ src/components/Footer/Styled.Footer.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const FooterTop = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  cursor: pointer;
+`;
+
+export const FooterDiv = styled.div`
+  width: 50%;
+  padding: 7rem;
+  border: 0.5px solid white;
+  text-align: center;
+`;
+
+export const FooterTitle = styled.div`
+  padding-top: 1rem;
+  font-size: ${({ size }) => size};
+  font-weight: 600;
+`;
+
+export const FooterUnder = styled.div`
+  width: 100%;
+  padding: 10rem;
+  color: black;
+  background-color: white;
+  text-align: center;
+`;
+
+export const FooterUderTitle = styled.div`
+  padding-top: 1rem;
+  font-size: ${({ size }) => size};
+  font-weight: 1000;
+`;
+
+export const FooterBottom = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding: 2rem 0;
+  color: black;
+  background-color: white;
+  border-top: 0.5px solid black;
+  font-size: 2rem;
+  font-weight: 600;
+`;
